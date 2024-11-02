14f0364e7b678d783b5e7abbe16604845ceb7c6d
Niall Antony <88824327+niallantony@users.noreply.github.com>
2024-11-02 16:30:56 +0900

    Set up basic Express project

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const path = require("node:path");
 
+//Routers
+
+
+const app = express();
+const assetsPath = path.join(__dirname, "public");
+
+process.on('uncaughtException', (err) => {
+    console.log(err);
+})
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(assetsPath));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.send("Hello, World");
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Classroom Manager - listening on port ${PORT}`);
+})
