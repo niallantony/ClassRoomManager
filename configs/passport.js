675d115ab0ef86b0c0943f1caa7f5c9b699717d4
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-02-21 12:56:31 +0900

    Removed needless logging

--- configs/passport.js
+++ configs/passport.js
@@ -1,45 +1,42 @@
 const passport = require("passport");
 const { User } = require("../model/query");
 const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcryptjs")
-
+const bcrypt = require("bcryptjs");
 
 passport.use(
-    new LocalStrategy(
-        {
-            usernameField:"email",
-        },
-        async (email, password, done) => {
-        try {
-            console.log("Email: ",email)
-            const db = User();
-            const user = await db.queryEmail(email);
-            if (!user) {
-                return done(null, false, {message: "Incorrect username"});
-            }
-            const match = await bcrypt.compare(password, user.password);
-            if (!match) {
-                return done(null, false, {message: "Incorrect password"});
-            }
-            console.log(user)
-            return done(null, user);
-        } catch(err) {
-            done(err);
+  new LocalStrategy(
+    {
+      usernameField: "email",
+    },
+    async (email, password, done) => {
+      try {
+        const db = User();
+        const user = await db.queryEmail(email);
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
         }
-    })
-)
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
-    done(null, user.teacher_id);
+  done(null, user.teacher_id);
 });
 
 passport.deserializeUser(async (id, done) => {
-    try {
-        const db = User();
-        const user = await db.queryId(id);
-        done (null, user);
-    } catch(err) {
-        done(err); 
-    }
-})
-
+  try {
+    const db = User();
+    const user = await db.queryId(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
--- controller/indexController.js
+++ controller/indexController.js
@@ -1,26 +1,25 @@
-const getIndex = (req,res) => {
-    res.send({
-        title:"ClassRoom Manager",
-        content:'welcome',
-    })
-    res.end();
-}
+const getIndex = (req, res) => {
+  res.send({
+    title: "ClassRoom Manager",
+    content: "welcome",
+  });
+  res.end();
+};
 
-const getDash = (req,res, next) => {
-    if (req.user) {
-        console.log(req.user);
-        console.log(req.session)
-        res.send({
-            title:"Welcome to the app",
-            user: req.user,
-        })
-    } else {
-        console.log("No session found...") 
-        next();
-    }
-}
+const getDash = (req, res, next) => {
+  if (req.user) {
+    res.send({
+      title: "Welcome to the app",
+      user: req.user,
+    });
+  } else {
+    console.log("No session found...");
+    next();
+  }
+};
 
 module.exports = {
-    getIndex,
-    getDash,
-}
+  getIndex,
+  getDash,
+};
+
